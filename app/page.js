4af2adf667c62a4465668762855607c215ea7052
kaunang-blip <kaunang@gmail.com>
2026-08-23 11:09:57 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import { findOilMatch } from '../lib/oilMatches'
-
+import { findOilMatch } from './lib/oilMatches'
 function formatKenteken(value) {
   return value.toUpperCase().replace(/[^A-Z0-9]/g, '').slice(0, 8)
 }
